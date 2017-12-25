c8f253d4c7f9400f8dd4712a7678814aacaa83f2
Aadam <aadimator@gmail.com>
2017-12-25 14:12:49 +0500

    Add Google Maps to MapFragment

--- khoji_app/app/src/main/java/com/aadimator/khoji/activities/MainActivity.java
+++ khoji_app/app/src/main/java/com/aadimator/khoji/activities/MainActivity.java
@@ -1,17 +1,21 @@
 package com.aadimator.khoji.activities;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.aadimator.khoji.R;
+import com.aadimator.khoji.fragments.MapFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MapFragment.OnFragmentInteractionListener {
 
     private TextView mTextMessage;
+    private Fragment mSelectedFragment = null;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -20,27 +24,46 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_map:
+                    mSelectedFragment = MapFragment.newInstance();
                     mTextMessage.setText(R.string.title_map);
-                    return true;
+                    break;
                 case R.id.navigation_contacts:
                     mTextMessage.setText(R.string.title_contacts);
-                    return true;
+                    break;
                 case R.id.navigation_account:
                     mTextMessage.setText(R.string.title_account);
-                    return true;
+                    break;
             }
-            return false;
+
+            changeFragment();
+            return true;
         }
     };
+
+    private void changeFragment() {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.main_fragment, mSelectedFragment)
+                .commit();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setTitle(getString(R.string.app_name));
+
         mTextMessage = (TextView) findViewById(R.id.message);
+        mSelectedFragment = MapFragment.newInstance();
+        changeFragment();
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
 }
--- khoji_app/app/src/main/java/com/aadimator/khoji/fragments/MapFragment.java
+++ khoji_app/app/src/main/java/com/aadimator/khoji/fragments/MapFragment.java
@@ -20,14 +20,6 @@ import com.aadimator.khoji.R;
  * create an instance of this fragment.
  */
 public class MapFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
@@ -35,31 +27,16 @@ public class MapFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MapFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MapFragment newInstance(String param1, String param2) {
+
+    public static MapFragment newInstance() {
         MapFragment fragment = new MapFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
